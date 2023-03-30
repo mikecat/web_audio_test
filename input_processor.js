32233b20587fcb2a2acb626a714d1c82c7f217c9
MikeCAT <mikecat@users.noreply.github.com>
2023-03-31 02:20:23 +0900

    add input test

---
+++ input_processor.js
@@ -0,0 +1,71 @@
+"use strict";
+
+class InputProcessor extends AudioWorkletProcessor {
+	constructor(...args) {
+		super(...args);
+		const processTime = 0.1; // s
+		const logSizeRaw = sampleRate * processTime;
+		const logBlockSize = Math.round(Math.sqrt(logSizeRaw));
+		const logSize = logBlockSize * logBlockSize;
+		this.logData = new Array(logSize);
+		this.logBlockData = new Array(logBlockSize);
+		for (let i = 0; i < this.logData.length; i++) {
+			this.logData[i] = 0;
+		}
+		for (let i = 0; i < this.logBlockData.length; i++) {
+			this.logBlockData[i] = {
+				"max": 0,
+				"min": 0,
+				"avg": 0,
+			};
+		}
+		this.logStartPos = 0;
+		this.elapsedFrames = 0;
+	}
+
+	process(inputs, outputs, parameters) {
+		const input = inputs[0][0]; // consider only first channel of first input
+		let ptr = this.logStartPos;
+		for (let i = 0; i < input.length; i++) {
+			this.logData[ptr] = input[i];
+			ptr++;
+			if(ptr >= this.logData.length) ptr = 0;
+		}
+		const newLogStartPos = ptr;
+		ptr--;
+		if (ptr < 0) ptr = this.logData.length - 1;
+		const startBlock = Math.floor(this.logStartPos / this.logBlockData.length);
+		const endBlock = Math.floor(ptr / this.logBlockData.length);
+		this.logStartPos = newLogStartPos;
+		for (let i = startBlock; ; ) {
+			const offset = this.logBlockData.length * i;
+			const firstValue = this.logData[offset];
+			const newData = {"max": firstValue, "min": firstValue, "avg": 0};
+			for (let j = 0; j < this.logBlockData.length; j++) {
+				const data = this.logData[offset + j];
+				if (newData.max < data) newData.max = data;
+				if (newData.min > data) newData.min = data;
+				newData.avg += data;
+			}
+			newData.avg /= this.logBlockData.length;
+			this.logBlockData[i] = newData;
+			if (i == endBlock) break;
+			i++;
+			if (i >= this.logBlockData.length) i = 0;
+		}
+		this.elapsedFrames += input.length;
+		if (this.elapsedFrames >= this.logData.length) {
+			this.elapsedFrames %= this.logData.length;
+			const resultData = {"max": this.logBlockData[0].max, "min": this.logBlockData[0].min, "avg": 0};
+			for (let i = 0; i < this.logBlockData.length; i++) {
+				if (resultData.max < this.logBlockData[i].max) resultData.max = this.logBlockData[i].max;
+				if (resultData.min > this.logBlockData[i].min) resultData.max = this.logBlockData[i].min;
+				resultData.avg += this.logBlockData[i].avg;
+			}
+			resultData.avg /= this.logBlockData.length;
+			this.port.postMessage(resultData);
+		}
+	}
+}
+
+registerProcessor("input-processor", InputProcessor);
--- web_audio_test.js
+++ web_audio_test.js
@@ -6,8 +6,16 @@ window.addEventListener("DOMContentLoaded", function() {
 	const durationInput = document.getElementById("duration-input");
 	const enqueueButton = document.getElementById("enqueue-button");
 
+	const recButton = document.getElementById("rec-button");
+	const amplitudeMeter = document.getElementById("amplitude-meter");
+	const minArea = document.getElementById("min-area");
+	const maxArea = document.getElementById("max-area");
+	const avgArea = document.getElementById("avg-area");
+
 	let audioContext = null;
 	let outputGenerator = null;
+	let sourceNode = null;
+	let inputProcessor = null;
 
 	playButton.addEventListener("click", async function() {
 		if (audioContext === null) audioContext = new AudioContext();
@@ -31,5 +39,33 @@ window.addEventListener("DOMContentLoaded", function() {
 				"duration": duration,
 			});
 		}
+	});
+
+	recButton.addEventListener("click", async function() {
+		if (audioContext === null) audioContext = new AudioContext();
+		if (sourceNode === null) {
+			try {
+				const source = await navigator.mediaDevices.getUserMedia({"audio": true});
+				sourceNode = audioContext.createMediaStreamSource(source);
+			} catch (error) {
+				alert(error.name + ": " + error.message);
+				return;
+			}
+		}
+		if (inputProcessor === null) {
+			await audioContext.audioWorklet.addModule("input_processor.js");
+			inputProcessor = new AudioWorkletNode(audioContext, "input-processor");
+			inputProcessor.channelCountMode = "explicit";
+			inputProcessor.channelCount = 1;
+			inputProcessor.port.onmessage = function(e) {
+				const data = e.data;
+				minArea.textContent = data.min;
+				maxArea.textContent = data.max;
+				avgArea.textContent = data.avg;
+				amplitudeMeter.value = Math.max(data.max - data.avg, data.avg - data.min);
+			}
+			sourceNode.connect(inputProcessor);
+		}
+		recButton.disabled = true;
 	});
 });
